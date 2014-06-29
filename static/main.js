b75bd47a952d8697df15d3657185c787b764b2f7
tonghs <tonghuashuai@gmail.com>
2014-06-29 09:54:36 +0800

    fix icon text

--- static/main.js
+++ static/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#s_home').css('color', '#4bbfc3');
+    $('#s_home').css('background-image', "url('images/logo_focus.png')");
     $('#fullpage').fullpage({
         slidesColor: ['#1ABC9C', '#8fb98b', '#3498DB', '#eae1c0', '#de564b', '#7BAABE', '#E67E22', '#2f2f2f', '#E74C3C'],
         anchors:['home', 'friends', 'lover', 'roommates', 'people', 'besties', 'classmates', 'about'],
@@ -7,14 +7,44 @@ $(document).ready(function() {
         easing: 'easeOutExpo',
         menu: '#menu',
         loopBottom: true,
-        slidesNavigation: true,			
+        slidesNavigation: true,
 
         //events
         afterLoad: function(anchorLink, index){
             $('#menu a').css('color', '#505050');
             $('#menu a').css('text-decoration', 'none');
-            $('#s_' + anchorLink).css('color', '#4bbfc3');
-            $('#s_' + anchorLink).css('text-decoration', 'underline');
+
+            $('#menu a').each(function(){
+                if ($(this).attr('id').indexOf(anchorLink) < 0){
+                    $(this).hover(function(){
+                        if ($(this).attr('id').indexOf('s_home') >=0){
+                            $(this).css('background-image', "url('images/logo_hover.png')");
+                        }else{
+                            $(this).css('color', '#000');
+                        }
+
+                    }, function(){
+                        if ($(this).attr('id').indexOf('s_home') >=0){
+                            $(this).css('background-image', "url('images/logo.png')");
+                        }else{
+                            $(this).css('color', '#505050');
+                        }
+
+                    })
+                }
+            });
+
+            if (anchorLink.indexOf('home') >= 0){
+                $('#s_home').css('background-image', "url('images/logo_focus.png')");
+            }else{
+                $('#s_home').css('background-image', "url('images/logo.png')");
+                $('#s_' + anchorLink).css('color', '#4bbfc3');
+                $('#s_' + anchorLink).css('text-decoration', 'underline');
+
+                $('#s_' + anchorLink).hover(function(){
+                    $(this).css('color', '#4bbfc3');
+                });
+            }
         }
     });
 });
